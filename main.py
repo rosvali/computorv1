92e7a5392d9e80c445ccc76ce3d72e8da4f82c60
rosvali <rosval6578@gmail.com>
2022-08-12 18:23:09 +0200

    solve second degree

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
-import equation
-import parse
-import maths
 import re
+from math import sqrt
 
 # todo handle X^-1 / X^1.2
+# todo sqrt function
 
 def swap_equation(lequation, requation):
     tmp = lequation
@@ -15,7 +14,10 @@ def swap_equation(lequation, requation):
 def print_equation(equation):
     i = len(equation) - 1
     while(i >= 0):
-        print("+", end = " ") if i != len(equation) - 1 and equation[i] > 0 else print("-", end = " ")
+        if i < len(equation) - 1 and equation[i] > 0:
+            print("+", end = " ")
+        elif equation[i] < 0:
+            print("-", end = " ")
         print(f"{abs(equation[i])} * X^{i}", end = " ")
         i -= 1
 
@@ -55,35 +57,47 @@ def parsing_arg(arg):
     equation = reduced_form(lequation, requation)
     return (equation)
 
+def print_degree(equation):
+    print(f"Polynomial degree: {len(equation) - 1}")
+    if len(equation) > 3:
+        print("The polynomial degree is stricly greater than 2, I can't solve.")
+    return(len(equation) - 1)
+
+def calc_delta(equation):
+    return(equation[1] ** 2 - 4 * equation[2] * equation[0])
+
+def second_degree(equation):
+    if len(equation) - 1 == 2:
+        delta = calc_delta(equation)
+        if delta > 0:
+            print("Discriminant is strictly positive, the two solutions are:")
+            x1 = (-equation[1] - sqrt(delta)) / (2 * equation[2])
+            x2 = (-equation[1] + sqrt(delta)) / (2 * equation[2])
+            print(x1)
+            print(x2)
+        elif delta == 0:
+            print("Discriminant is nul, the solution is:")
+            x1 = -equation[1]/(2*equation[2])
+            print(x1)
+        else:
+            print("Discriminant is negatif. No solution.")
+
+def solve_equation(equation):
+    degree = len(equation) - 1
+    if degree == 2:
+        second_degree(equation)
+
 def main():
     equation = []
     if len(sys.argv) == 2:
         equation = parsing_arg(sys.argv[1])
+        if print_degree(equation) <= 2:
+            solve_equation(equation)
     else:
         print("Error: Need one argument")
 
 main()
 
-# arg = parse.Parse("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
-
-# print(arg.reduced_form("5 * X^0 + 4 * X^1 - 9.3 * X^2", "1 * X^0")) #4.0 * X^0 + 4 * X^1 - 9.3 * X^2
-
-# a, b, c = arg.create_equation("4.0 * X^0 + 4 * X^1 - 9.3 * X^2")
-# eq = equation.Equation(a, b, c)
-
-# # eq = equation.Equation("-9.3", "4", "4.0")
-
-# print("a: %s" % (eq.a))
-# print("b: %s" % (eq.b))
-# print("c: %s" % (eq.c))
-
-# print(f"Delta = {eq.discriminant()}")
-
-# print(f"Racine(s) = {eq.polynome()}")
-
-# print("a is negatif? %s" % (maths.is_negatif(eq.a)))
-# print("b is negatif? %s" % (maths.is_negatif(eq.b)))
-# print("c is negatif? %s" % (maths.is_negatif(eq.c)))
-
-# print ("Equation:", end = " ")
-# eq.display_equation()
+# 2 * X^2 + 8 * X^1 + 8 * X^0 = 0 <== delta = 0
+# 5 * X^2 - 2 * X^1 + 1 * X^0 = 0 <== delta < 0
+# 9 * X^2 - 5 * X^1 + 2 * X^0 = 0 <== delta > 0
