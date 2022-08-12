282ea98c94bb4bd8a4aca5d1196b70197382e094
rosvali <rosval6578@gmail.com>
2022-08-12 13:04:00 +0200

    reduce form

--- main.py
+++ main.py
@@ -4,7 +4,33 @@ import parse
 import maths
 import re
 
-# todo handle power negatif and float
+# todo handle X^-1 / X^1.2
+
+def swap_equation(lequation, requation):
+    tmp = lequation
+    lequation = requation
+    requation = tmp
+    return(lequation, requation)
+
+def print_equation(equation):
+    i = len(equation) - 1
+    while(i >= 0):
+        print("+", end = " ") if i != len(equation) - 1 and equation[i] > 0 else print("-", end = " ")
+        print(f"{abs(equation[i])} * X^{i}", end = " ")
+        i -= 1
+
+def reduced_form(lequation, requation):
+    i = 0
+    if len(lequation) < len(requation):
+        lequation, requation = swap_equation(lequation, requation)
+    for coeff in requation:
+        lequation[i] += coeff
+        requation[i] = 0
+        i += 1
+    print("Reduced form:", end = " ")
+    print_equation(lequation)
+    print("= 0")
+    return(lequation)
 
 def table_arg(side_equation):
     index_tokens = []
@@ -14,31 +40,27 @@ def table_arg(side_equation):
             if re.match("\+|-", side_equation[i - 3]):
                 side_equation[i - 2] = side_equation[i - 3] + side_equation[i - 2]
             if len(index_tokens) > int(element[-1]):
-                index_tokens[float(element[-1])] += float(side_equation[i - 2])
+                index_tokens[int(element[-1])] += float(side_equation[i - 2])
             else:
                 while len(index_tokens) < float(element[-1]):
                     index_tokens.insert(len(index_tokens) - 1, 0)
-                index_tokens.insert(float(element[-1]), float(side_equation[i - 2]))
+                index_tokens.insert(int(element[-1]), float(side_equation[i - 2]))
         i += 1
-    print(index_tokens)
+    return(index_tokens)
 
-def parsing_argv(arg):
+def parsing_arg(arg):
     res = arg.split(" = ")
-    lequation = re.split(" ", res[0])
-    requation = re.split(" ", res[1])
-    print(lequation, requation)
-    lequation = table_arg(lequation)
-    requation = table_arg(requation)
-    
+    lequation = table_arg(re.split(" ", res[0]))
+    requation = table_arg(re.split(" ", res[1]))
+    equation = reduced_form(lequation, requation)
+    return (equation)
+
 def main():
-    parsing = ""
-    equat = ""
+    equation = []
     if len(sys.argv) == 2:
-        print(sys.argv[1])
-        parsing_argv(sys.argv[1])
-        # parsing = parse.Parse(sys.argv[1]).parsing_equation()
+        equation = parsing_arg(sys.argv[1])
     else:
-        print("Error: Need an argument")
+        print("Error: Need one argument")
 
 main()
 
